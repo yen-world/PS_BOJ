6eb5dfe23283cb591994891203cb565e00db2f9a
yen-world <96435899+yen-world@users.noreply.github.com>
2023-11-26 17:26:23 +0900

    30676 AC

---
+++ 30000~30999/30676. 이 별은 무슨 색일까.py
@@ -0,0 +1,16 @@
+g = int(input())
+
+if 620 <= g <= 780 :
+    print("Red")
+elif 590 <= g < 620 :
+    print("Orange")
+elif 570 <= g < 590 :
+    print("Yellow")
+elif 495 <= g < 570 :
+    print("Green")
+elif 450 <= g < 495 :
+    print("Blue")
+elif 425 <= g < 450 :
+    print("Indigo")
+elif 380 <= g < 425 :
+    print("Violet")
